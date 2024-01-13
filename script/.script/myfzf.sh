57e1078f83d86bd6603bc999ec22ff78460ab9a7
thuang323 <taylorhuang0323@gmail.com>
2024-01-13 01:06:01 -0500

    updates nvim, myfzf, and yabairc

--- script/.script/myfzf.sh
+++ script/.script/myfzf.sh
@@ -1,10 +1,11 @@
 return_search_file=$({
-               find ~/Downloads -type f;
-               find ~/Documents -type f;
+               # find ~/Downloads -type f;
+               # find ~/Documents -type f;
                find ~/Purdue -type f;
                find ~/Desktop -type f;
                find ~/coding -type f;
                find ~/dotfiles -type f;
+               find ~/Notes -type f;
              } | fzf --height=90%)
 
 if [ -z $return_search_file ] # check if the file name is an empty string, such as, ctrl-c will cause an empty string
