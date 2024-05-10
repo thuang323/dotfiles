e93c029e624ac8832a555a0cfa1b466309de73a4
Taylor Huang <taylorhuang@taylors-mbp.mynetworksettings.com>
2024-05-10 06:55:48 -0400

    adds wezterm

---
+++ wezterm/.config/wezterm/colors.lua
@@ -0,0 +1,81 @@
+local cyberdream = {
+  -- fg = "#fcfcfc",
+  fg = "#e6e6e6",
+  bg = "#16181a",
+
+  -- cursor_bg = "#ffffff",
+  -- cursor_fg = "#16181a",
+  -- cursor_border = "#ffffff",
+  -- cursor_bg = "#b2b2b2",
+  -- cursor_fg = "#e5e5e5",
+  -- cursor_border = "#b2b2b2",
+  cursor_bg = "#e2e2e2",
+  cursor_fg = "#000000",
+  cursor_border = "#e2e2e2",
+
+  selection_fg = "#e6e6e6",
+  selection_bg = "#3c4048",
+
+  scrollbar_thumb = "#16181a",
+  split = "#16181a",
+
+  black = "#16181a",
+  white = "#fcfcfc",
+  grey = "#7b8496",
+  blue = "#4aacfc",
+  green = "#5eff6c",
+  cyan = "#4DF0FF",
+  red = "#f65866",
+  yellow = "#fad773",
+  magenta = "#ff5ef1",
+  pink = "#ff5ea0",
+  orange = "#f79811",
+  purple = "#cd6beb",
+  bgAlt = "#1e2124",
+
+
+  ansi = {
+    "#16181a",
+    "#f65866",
+    "#5eff6c",
+    "#fad773",
+    "#4aacfc",
+    "#cd6beb",
+    "#4DF0FF",
+    "#fcfcfc",
+  },
+
+  brights = {
+    "#3c4048",
+    "#f65866",
+    "#5eff6c",
+    "#fad773",
+    "#4aacfc",
+    "#cd6beb",
+    "#4DF0FF",
+    "#fcfcfc",
+  },
+  indexed = {
+    [16] = "#ffbd5e",
+    [17] = "#f65866"
+  },
+  -- foreground = "#ffffff",
+  -- background = "#16181a",
+  --
+  -- cursor_bg = "#ffffff",
+  -- cursor_fg = "#16181a",
+  -- cursor_border = "#ffffff",
+  --
+  -- selection_fg = "#ffffff",
+  -- selection_bg = "#3c4048",
+  --
+  -- scrollbar_thumb = "#16181a",
+  -- split = "#16181a",
+  --
+  -- ansi = { "#16181a", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+  -- brights = { "#3c4048", "#ff6e5e", "#5eff6c", "#f1ff5e", "#5ea1ff", "#bd5eff", "#5ef1ff", "#ffffff" },
+  -- indexed = { [16] = "#ffbd5e", [17] = "#ff6e5e" },
+}
+
+
+return cyberdream
