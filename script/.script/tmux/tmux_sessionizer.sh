152bf93794cdacb20a9d17bca23a742ae2c4db57
thuang323 <taylorhuang0323@gmail.com>
2025-01-13 20:11:43 -0500

    update

--- script/.script/tmux/tmux_sessionizer.sh
+++ script/.script/tmux/tmux_sessionizer.sh
@@ -37,32 +37,29 @@ else
 fi
 
 if [[ -z $selected ]]; then
-  if [[ -z $TMUX ]]; then
-    return
-  else
-    exit 0
-  fi
+  exit 0
 fi
 
-selected_name=$(basename "$selected" | tr . _)
-selected_name=$(echo "$selected_name" | tr ' ' '_')
-tmux_running=$(pgrep tmux)
+# selected_name=$(basename "$selected" | tr . _)
+# get last two directories in path
+# selected_name=$(basename "$(dirname "$selected")")/$(basename "$selected")
+selected_name=$(basename "$selected")
 
 if [[ $selected_name = "taylorhuang" ]]; then
   selected_name="home"
 fi
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s "$selected_name" -c "$selected"
-  if [[ -z $tmux_running ]]; then
-    return
-  else
-    exit 0
-  fi
+# if tmux is not attached, create session or attach to session
+if [[ -z $TMUX ]]; then
+  tmux new-session -A -s "$selected_name" -c "$selected"
+
+# if tmux is attached and session doesn't exist, create session
+elif ! tmux has-session -t=$selected_name 2> /dev/null; then
+  tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    tmux new-session -ds "$selected_name" -c "$selected"
-fi
+# replace . with _ in selected_name
+selected_name=${selected_name//./_}
 
+# if tmux is attached, switch to session
 tmux switch-client -t "$selected_name"
