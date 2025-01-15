2ab95812207d8a0fad44b2b6b6862f4c8c58f9bc
thuang323 <taylorhuang0323@gmail.com>
2025-01-15 01:31:40 -0500

    update

---
+++ bash/.bashrc
@@ -0,0 +1,33 @@
+# for ssh usage
+
+# this file is processed on each interactive invocation of bash
+
+# avoid problems with scp -- don't process the rest of the file if non-interactive
+[[ $- != *i* ]] && return
+
+# increase history size
+HISTSIZE=50000
+HISTFILESIZE=50000
+
+
+# Generated for envman. Do not edit.
+[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
+
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# add to path
+export EDITOR=nvim
+export PATH="$HOME/usr/bin:$PATH"
+
+eval "$(starship init bash)"
+
+# aliases
+alias mail=mailx
+alias vi="nvim"
+alias ta="tmux attach"
+alias tn="tmux new"
+alias tls="tmux ls"
+alias ts="~/.script/tmux/tmux_sessionizer.sh"
+alias sd="source ~/.script/find_directory.sh"         # find all directory under home in specific directory
+alias sud="source ~/.script/find_under_directory.sh"  # find all directory under current directory
+
--- script/.script/find_directory.sh
+++ script/.script/find_directory.sh
@@ -6,17 +6,27 @@ for dir in "${EXCLUDE_DIRS[@]}"; do
   EXCLUDE_ARGS+=(-path "$dir" -o)
 done
 
-dir_name=$({
-  echo ~/;
-  echo ~/Downloads/;
-  echo ~/Desktop/;
-  echo ~/resume/;
-  echo ~/test/;
-  echo ~/dotfiles/;
-  find ~/Purdue -maxdepth 3 -type d;
-  find ~/coding -maxdepth 2 -type d \( "${EXCLUDE_ARGS[@]:0:${#EXCLUDE_ARGS[@]}-1}" \) -prune -o -type d -print;
-  find ~/dotfiles -mindepth 1 -maxdepth 3 -type d \( "${EXCLUDE_ARGS[@]:0:${#EXCLUDE_ARGS[@]}-1}" \) -prune -o -type d -print;
-} | fzf --height=90%)
+if [[ $HOME = "/Users/taylorhuang" ]]; then
+  dir_name=$({
+    echo ~/;
+    echo ~/Downloads/;
+    echo ~/Desktop/;
+    echo ~/resume/;
+    echo ~/test/;
+    echo ~/dotfiles/;
+    echo ~/.config/nvim/;
+    find ~/Purdue -maxdepth 3 -type d;
+    find ~/coding -maxdepth 2 -type d \( "${EXCLUDE_ARGS[@]:0:${#EXCLUDE_ARGS[@]}-1}" \) -prune -o -type d -print;
+    # find ~/dotfiles -mindepth 1 -maxdepth 3 -type d \( "${EXCLUDE_ARGS[@]:0:${#EXCLUDE_ARGS[@]}-1}" \) -prune -o -type d -print;
+  } | fzf --height=90%)
+else
+  dir_name=$({
+    echo ~/;
+    echo ~/Downloads/;
+    echo ~/dotfiles/;
+    echo ~/.config/nvim/;
+  } | fzf --height=90%)
+fi
 
 
 if [[ -z $dir_name ]]; then
